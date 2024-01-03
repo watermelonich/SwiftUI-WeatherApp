54526aa3bbae0ee4113ec9421bb43c546f06aa48
Nich <128910037+watermelonich@users.noreply.github.com>
2024-01-03 09:42:34 +0100

    Initial commit

---
+++ Weather/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Nich on 12/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Nich on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
